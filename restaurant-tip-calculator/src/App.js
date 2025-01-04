fc3a396a28d3a165d9163bd851affbe0a17c45bc
ev3test <9vashini@gmail.com>
2025-01-03 20:46:57 -0500

    PrivateRoute complete, app.js complete.

--- restaurant-tip-calculator/src/App.js
+++ restaurant-tip-calculator/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Signup from 'Signup';
+import Login from 'login';
+import HomePage from 'HomePage';
+import PrivateRoute from 'PrivateRoute';
+import { AuthProvider } from 'AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
