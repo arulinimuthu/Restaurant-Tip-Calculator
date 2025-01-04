3dc96d0f30799c3751537e46fb78eda68c88da0a
ev3test <9vashini@gmail.com>
2025-01-03 20:13:15 -0500

    Completed Login Functionality + Testing Pending

--- restaurant-tip-calculator/src/login.js
+++ restaurant-tip-calculator/src/login.js
@@ -1,2 +1,69 @@
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
+import { useState } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (email, password) => {
+        try {
+            await signInWithEmailAndPassword (auth, email, password);
+            alert ("Logged in successfully!");
+        } 
+        catch (e) {
+            console.error (e.message);
+        }
+    }
+    const handleGoogleLogin = async () => {
+        try {
+            const provider = new GoogleAuthProvider ();
+
+            await signInWithPopup (auth, provider);
+            alert ("Logged in successfully with Google!");
+        } 
+        catch (e) {
+            console.error (e.message);
+         }
+    };
+
+    return (
+        <div>
+            <h1>
+                Login
+            </h1>
+            <button onClick = {handleGoogleLogin}>
+                Login With Google
+            </button>
+            <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Login</button>
+      </form>
+        </div>
+    );
+
+
+};
+
+export default Login;
